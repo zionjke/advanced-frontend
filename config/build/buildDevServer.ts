303fe2c8323672e8a63530126d8294e3a55c0182
vart <vart@jooble.com>
2023-05-13 18:13:29 +0300

    add router, add component lazy loading, code-splitting

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -4,6 +4,7 @@ import type {Configuration as DevServerConfigurations} from 'webpack-dev-server'
 export const buildDevServer = (options: BuildOptions): DevServerConfigurations => {
     return {
         port: options.port,
-        open: true
+        open: true,
+        historyApiFallback: true // прокси реквестов через index page
     }
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import './index.scss'
-import {Counter} from "./components/Counter";
+import {Link, Route, Routes} from 'react-router-dom';
+import {AboutPage} from "./pages/About/About.lazy";
+import {MainPage} from "./pages/Main/Main.lazy";
+
 
 const App = () => {
     return (
         <div className="app">
-            dadadqda
-            <Counter/>
+            <Link to='/'>Main</Link>
+            <Link to='/about'>About</Link>
+            {/* Suspense используется для асинхронной подгрзузки компонентов */}
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path="/" element={<MainPage/>}/>
+                    <Route path="/about" element={<AboutPage/>}/>
+                    <Route/>
+                </Routes>
+            </Suspense>
         </div>
     );
 };
