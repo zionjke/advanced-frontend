104e7f424097389a894e9a253273341c17a6ec36
vart <vart@jooble.com>
2023-05-25 14:06:10 +0300

    disabled bundleAnalyzer on prod env

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,23 +1,32 @@
-import HTMLWebpackPlugin from 'html-webpack-plugin';
-import webpack from 'webpack';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import * as HTMLWebpackPlugin from 'html-webpack-plugin';
+import * as webpack from 'webpack';
+import * as MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
 
-export const buildPlugins = ({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] => [
-    new HTMLWebpackPlugin({
-        template: paths.html, // говорим брать за темплейт наш хтмл файл.
-    }),
-    new MiniCssExtractPlugin({
-        filename: 'css/[name].[contenthash:8].css',
-        chunkFilename: 'css/[name].[contenthash:8].css',
-    }), // нужен для того чтобы создавались отдельно цсс файлы в сборке
-    new webpack.ProgressPlugin(), // прогресс при сборке проекта
-    new webpack.DefinePlugin({ // для создания глобальных переменных
-        __IS_DEV__: JSON.stringify(isDev),
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-    new BundleAnalyzerPlugin({
-        openAnalyzer: false,
-    }),
-];
+export const buildPlugins = ({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] => {
+    const plugins = [
+        new HTMLWebpackPlugin({
+            template: paths.html, // говорим брать за темплейт наш хтмл файл.
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].[contenthash:8].css',
+            chunkFilename: 'css/[name].[contenthash:8].css',
+        }), // нужен для того чтобы создавались отдельно цсс файлы в сборке
+        new webpack.ProgressPlugin(), // прогресс при сборке проекта
+        new webpack.DefinePlugin({ // для создания глобальных переменных
+            __IS_DEV__: JSON.stringify(isDev),
+        }),
+    ];
+
+    if (isDev) {
+        plugins.push(
+            new webpack.HotModuleReplacementPlugin(),
+            new BundleAnalyzerPlugin({
+                openAnalyzer: false,
+            }),
+        );
+    }
+
+    return plugins;
+};
