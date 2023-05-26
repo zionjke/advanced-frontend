9168a27148efa5798465655ea77e289b6b7e3a7e
vart <vart@jooble.com>
2023-05-26 17:57:06 +0300

    fix sidebar test

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,14 +1,14 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import { renderWithTranslation } from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
+import { fireEvent, screen } from '@testing-library/react';
+import { componentRender } from 'shared/config/tests/componentRender/componentRender';
 import { Sidebar } from './Sidebar';
 
 describe('Sidebar component', () => {
     test('render Sidebar', () => {
-        renderWithTranslation(<Sidebar />);
+        componentRender(<Sidebar />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
     test('Sidebar open', () => {
-        renderWithTranslation(<Sidebar />);
+        componentRender(<Sidebar />);
         const toggleBtn = screen.getByTestId('sidebar-toggle');
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
         fireEvent.click(toggleBtn);
